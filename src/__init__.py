7fda66010a337114f92eabb5a52ac38bfdf4f514
arvectum1 <cor6iqhf50@aureusempire.shop>
2026-08-24 19:05:44 +0300

    DP-CUST-016: install readiness calibration globally

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,9 @@
 from src.customer_feedback_14 import install_customer_feedback_14
 from src.customer_feedback_14_promokood import install_promokood_block_parser
 from src.customer_feedback_15 import install_customer_feedback_15
+from src.customer_feedback_16 import install_customer_feedback_16
 
 install_customer_feedback_14()
 install_promokood_block_parser()
 install_customer_feedback_15()
+install_customer_feedback_16()
